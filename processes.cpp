dc558b2bdcb346c1977c0d62767a869b2fee184c
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2019-01-08 17:14:29 -0800

    basic setup for os

---
+++ processes.cpp
@@ -0,0 +1,7 @@
+/*
+Code a C++ program, named processes.cpp that receives one argument, 
+(i.e., argv[1]) upon its invocation and searches how many processes 
+whose name is given in argv[1] are running on the system where your program has been invoked. 
+To be specific, your program should demonstrate the same behavior as: 
+$ ps -A | grep argv[1] | wc - l 
+*/
