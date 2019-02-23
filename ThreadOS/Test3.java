2547e3cf3c25e0d89aa1b7dfa1154fda83eff449
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2019-02-22 18:30:00 -0800

    Have an idea of what to learn/do from here. Parallel test 2 structure and methods.

--- ThreadOS/Test3.java
+++ ThreadOS/Test3.java
@@ -27,17 +27,34 @@ class Test3 extends Thread {
         long startTime = (new Date( ) ).getTime( ); 
 
         
+        for(int i = 0; i < pairs; i++){
+            SysLib.exec( args1 );
+            SysLib.exec( args2 );
+        
+        }
 
         // write code to exec pairs number of computationally intensive and disk intensive  
 
         // so you will have pairs*2 threads 
-        
+        /*
+    public static String[] stringToArgs( String s ) {
+        StringTokenizer token = new StringTokenizer( s," " );
+        String[] progArgs = new String[ token.countTokens( ) ];
+        for ( int i = 0; token.hasMoreTokens( ); i++ ) {
+            progArgs[i] = token.nextToken( );
+        }
+	return progArgs;
+    }
+        */
+
         
 
         // write code to wait for each test to finish (i.e., you need to join pairs*2 times) 
 
-    
-
+        for(int j = 0; j < pairs; j++){
+            SysLib.join();
+            SysLib.join();
+        }
         long endTime = (new Date( ) ).getTime( ); 
 
     
---
+++ ThreadOS/TestThread3a.java
@@ -0,0 +1,3 @@
+Class  TestThread3a{
+
+}
